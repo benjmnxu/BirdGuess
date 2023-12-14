ce17e72107302ba17dc9275822321f7f503d4c8a
benjmnxu <123025188+benjmnxu@users.noreply.github.com>
2023-12-14 18:16:10 -0500

    otherCountries

--- backend/routes.js
+++ backend/routes.js
@@ -39,7 +39,6 @@ const birdcountryfact = async function (req, res) {
 const newBird = async function(req, res) {
     const prev_names = req.query.prevNames === undefined ? "": req.query.prevNames
     const prev_countries = req.query.prevCountries === undefined ? "": req.query.prevCountries
-    const random_offset = Math.floor(Math.random() * 697610)
     connection.query(`
     SELECT scientificName, accessURI
     FROM birdData
@@ -57,9 +56,73 @@ const newBird = async function(req, res) {
       });
 }
 
+const otherCountries = async function(req, res) {
+    const answer_country = req.params.country
+    connection.query(`
+    SELECT DISTINCT country
+    FROM birdData
+    WHERE country != '${answer_country}'
+    ORDER BY RAND()
+    LIMIT 4
+    `, (err, data) => {
+        if (err || data.length === 0) {
+            console.log(err);
+            res.json({});
+        } else {
+            res.json(data)
+        }
+      });
+}
+
 const birdAndFactsByRegion = async function(req, res) {
+    /*
+    Given a region, return most popular bird of each country with associated facts
+    */
     const region = req.query.region
-    const key_facts = tuple(req.params.key_facts)
+    const key_facts = req.params.key_facts
+    const year = req.params.year
+    connection.query(`
+    WITH tmp1(country_name) AS (
+        SELECT countryName
+        FROM countryRegion
+        WHERE region = '${region}'
+    ),
+        raw_birds(bird_name, country, bird_freq) AS (
+          SELECT scientificName, b.country, COUNT(1) as birdFreq
+          FROM birdData b JOIN tmp1 t ON t.country_name = b.country AND b.scientificName != 'Mystery mystery'
+          GROUP BY scientificName
+        ),
+        birds(bird_name, country, bird_freq, rnk) AS (
+            SELECT bird_name, country, bird_freq, ROW_NUMBER() over (PARTITION BY country order by bird_freq DESC)
+            FROM raw_birds
+        ),
+        tmp2(indicator_code, unitOfMeasure) AS (
+            SELECT indicatorCode, unitOfMeasure
+            FROM worldBankIndicators
+            WHERE indicatorName IN ${key_facts}
+        )
+    SELECT bird_name, countryName, value, indicatorCode, bird_freq, rnk
+    FROM worldBankData w
+        JOIN tmp1 t1 ON w.countryName = t1.country_name
+        JOIN tmp2 t2 ON w.indicatorCode = t2.indicator_code
+        JOIN birds b ON b.country = t1.country_name
+    WHERE year = ${year} AND rnk = 1
+    `, (err, data) => {
+        if (err || data.length == 0) {
+            console.log(err)
+            res.json({})
+        } else {
+            res.json(data)
+        }
+    });
+}
+
+const birdCountriesFacts = async function(req, res) {
+    /*
+    Given a bird, return countries where bird has been recorded with associated facts
+    */
+    const region = req.query.region
+    const key_facts = req.params.key_facts
     const year = req.params.year
     connection.query(`
     WITH tmp1(country_name) AS (
@@ -98,11 +161,10 @@ const birdAndFactsByRegion = async function(req, res) {
 }
 
 
-
-
 module.exports = {
     newBird,
-    birdAndFactsByRegion
+    birdAndFactsByRegion,
+    otherCountries
   }
 
 //Out of all countries the user has seen/guessed right, rank the countries by those with the most bird sounds
