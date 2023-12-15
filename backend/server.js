5985351551a3f50bde463da6b6f97711a7009980
Justin Sun <justinsunyt@gmail.com>
2023-12-15 01:34:29 -0500

    updated routes and server

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,11 @@ app.use(
 // provide their handlers that we implemented in routes.js
 app.get("/newbird", routes.newBird);
 app.get("/othercountries/:country", routes.otherCountries);
-app.get("/countryfact/:countryName", routes.countryFact);
-app.get("/birdandfactsbyregion", routes.birdAndFactsByRegion);
+app.get("/countryfact/:country", routes.countryFact);
 app.get("/birdcountriesfacts", routes.birdCountriesFacts);
-app.get("/genusToEnvCountry", routes.genusToCountry);
-app.get("/genusToYear", routes.genusToYear);
-app.get("/diffGenus", routes.diffGenus);
+app.get("/genustocountry", routes.genusToCountry);
+app.get("/genustoyear", routes.genusToYear);
+app.get("/diffgenus", routes.diffGenus);
 
 app.listen(config.server_port, () => {
   console.log(
