9820161b8d6b26a35ebb5f9c8a64d95d0cfd9ccd
Kevin Lu <kevinlu625@gmail.com>
2023-12-14 19:20:16 -0500

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,6 @@ app.use(
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
 app.get("/newbird", routes.newBird);
-// app.get('/random', routes.random);
-// app.get('/song/:song_id', routes.song);
-// app.get('/album/:album_id', routes.album);
-// app.get('/albums', routes.albums);
-// app.get('/album_songs/:album_id', routes.album_songs);
-// app.get('/top_songs', routes.top_songs);
-// app.get('/top_albums', routes.top_albums);
-// app.get('/search_songs', routes.search_songs);
 
 app.listen(config.server_port, () => {
   console.log(
