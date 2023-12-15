1eeca17e6555d853b43ae8abf234f8783949c554
Justin Sun <justinsunyt@gmail.com>
2023-12-15 02:21:26 -0500

    added country bird and fact and region birds and facts (todo: make region birds and facts AVG value)

--- backend/routes.js
+++ backend/routes.js
@@ -21,7 +21,7 @@ const newBird = async function (req, res) {
     req.query.prevCountries === undefined ? "" : req.query.prevCountries;
   connection.query(
     `
-    SELECT scientificName, country, accessURI
+    SELECT scientificName, country, accessURI, id
     FROM birdData
     WHERE scientificName NOT IN ('${prev_names}')
         AND country NOT IN ('${prev_countries}')
@@ -84,50 +84,6 @@ const countryFact = async function (req, res) {
 };
 
 // END OF ROUND
-
-// Given a bird, return countries where bird has been recorded with associated facts
-const birdCountriesFacts = async function (req, res) {
-  const region = req.query.bird;
-  const facts = req.query.facts;
-  const year = req.query.year;
-  connection.query(
-    `
-    WITH tmp1(country_name) AS (
-        SELECT countryName
-        FROM countryRegion
-        WHERE region = '${region}'
-    ),
-        raw_birds(bird_name, country, bird_freq) AS (
-          SELECT scientificName, b.country, COUNT(1) as birdFreq
-          FROM birdData b JOIN tmp1 t ON t.country_name = b.country AND b.scientificName != 'Mystery mystery'
-          GROUP BY scientificName
-        ),
-        birds(bird_name, country, bird_freq, rnk) AS (
-            SELECT bird_name, country, bird_freq, ROW_NUMBER() over (PARTITION BY country order by bird_freq DESC)
-            FROM raw_birds
-        ),
-        tmp2(indicator_code, unitOfMeasure) AS (
-            SELECT indicatorCode, unitOfMeasure
-            FROM worldBankIndicators
-            WHERE indicatorName IN ${facts}
-        )
-    SELECT bird_name, countryName, value, indicatorCode, bird_freq, rnk
-    FROM worldBankData w
-        JOIN tmp1 t1 ON w.countryName = t1.country_name
-        JOIN tmp2 t2 ON w.indicatorCode = t2.indicator_code
-        JOIN birds b ON b.country = t1.country_name
-    WHERE year = ${year} AND rnk = 1
-    `,
-    (err, data) => {
-      if (err || data.length == 0) {
-        console.log(err);
-        res.json({});
-      } else {
-        res.json(data);
-      }
-    }
-  );
-};
 
 // Given a list of genus already seen by the player,
 // list the genus name and the country with the max average value across metrics in the
@@ -230,12 +186,92 @@ const diffGenus = async function (req, res) {
   );
 };
 
+// Given a country, select a random bird from that country as well as a random indicator and its corresponding average value
+const randomCountryBirdAndFact = async function (req, res) {
+  const countryName = req.params.countryName;
+  connection.query(
+    `
+  WITH countryAverageIndicator AS (
+  SELECT countryName, indicatorName, AVG(value) averageValue
+  FROM worldBankData wbd JOIN worldBankIndicators wbi ON wbd.indicatorCode = wbi.indicatorCode
+  WHERE countryName = ${countryName}
+  GROUP BY indicatorName
+  ORDER BY RAND()
+  ),
+  birdNameCountry AS (
+  SELECT id, vernacularName, country
+  FROM birdData JOIN birdSpecies bS on bS.scientificName = birdData.scientificName
+  WHERE country = ${countryName}
+  ORDER BY RAND()
+  LIMIT 1
+  )
+  SELECT id, vernacularName, country, indicatorName, averageValue
+  FROM birdNameCountry bc JOIN countryAverageIndicator cAI ON bc.country = cAI.countryName
+  LIMIT 1;
+  `,
+    (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json({});
+      } else {
+        res.json(data[0]);
+      }
+    }
+  );
+};
+
+//given a region, a set of indicators, and a year, find the most common birds recorded in each of the countries
+//in that region as well as the associated indicator values (as specified)
+const regionBirdsAndFacts = async function (req, res) {
+  const region = req.query.region;
+  const facts = tuple(req.params.facts);
+  const year = req.params.year;
+  connection.query(
+    `
+  WITH tmp1(country_name) AS (
+  SELECT countryName
+  FROM countryRegion
+  WHERE region = '${region}'
+  ),
+  raw_birds(bird_name, country, bird_freq) AS (
+  SELECT scientificName, b.country, COUNT(1) as birdFreq
+  FROM birdData b JOIN tmp1 t ON t.country_name = b.country AND b.scientificName != 'Mystery mystery'
+  GROUP BY scientificName
+  ),
+  birds(bird_name, country, bird_freq, rnk) AS (
+  SELECT bird_name, country, bird_freq, ROW_NUMBER() over (PARTITION BY country order by bird_freq DESC)
+  FROM raw_birds
+  ),
+  tmp2(indicator_code, unitOfMeasure) AS (
+  SELECT indicatorCode, unitOfMeasure
+  FROM worldBankIndicators
+  WHERE indicatorName IN ${facts}
+  )
+  SELECT bird_name, countryName, value, indicatorCode, bird_freq, rnk
+  FROM worldBankData w
+  JOIN tmp1 t1 ON w.countryName = t1.country_name
+  JOIN tmp2 t2 ON w.indicatorCode = t2.indicator_code
+  JOIN birds b ON b.country = t1.country_name
+  WHERE year = ${year} AND rnk = 1
+  `,
+    (err, data) => {
+      if (err || data.length == 0) {
+        console.log(err);
+        res.json({});
+      } else {
+        res.json(data);
+      }
+    }
+  );
+};
+
 module.exports = {
   newBird,
   otherCountries,
   countryFact,
-  birdCountriesFacts,
   genusToCountry,
   genusToYear,
   diffGenus,
+  randomCountryBirdAndFact,
+  regionBirdsAndFacts,
 };
